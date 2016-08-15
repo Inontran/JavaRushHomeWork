2b789e770288cd41306d3d1284c919957d7e66c5
Inontran <braginsasha1995@mail.ru>
2016-08-15 19:52:07 +0900

    level19.lesson 10. save

--- src/com/javarush/test/level19/lesson10/bonus03/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus03/Solution.java
@@ -26,10 +26,7 @@ text2>text1</tag>
 text1, text2 могут быть пустыми
 */
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -50,32 +47,74 @@ public class Solution {
             DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             // Создается дерево DOM документа из файла
             Document document = documentBuilder.parse(consoleReader.readLine());
+//            Document document = documentBuilder.parse("C:\\Users\\sasha\\IdeaProjects\\JavaRushHomeWork\\out.txt");
 
             // Получаем корневой элемент
             Node root = document.getDocumentElement();
-            // Просматриваем все подэлементы корневого
-            NodeList tags = root.getChildNodes();
-            for (int i = 0; i < tags.getLength(); i++){
-                Node tag = tags.item(i);
-                System.out.println(printTag(tag));
+            //если искомый(required) тег корневой, то печатаем его на экран, иначе
+            if (root.getNodeName().equals(args[0])) System.out.println(tagConstructor(root));
+            else
+            {
+                // Просматриваем все подэлементы корневого
+                NodeList tags = root.getChildNodes();
+                printTags(tags, args[0]);
             }
+
+            if (consoleReader!=null) consoleReader.close();
         }
-        catch (SAXException e)
-        {
-            e.printStackTrace();
-        }
-        catch (ParserConfigurationException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
+        catch (SAXException | ParserConfigurationException | IOException e)
         {
             e.printStackTrace();
         }
     }
 
-    public static String printTag(Node node){
-        String tag = "";
-        return tag = "<" + node.getNodeName() + ">" + node.getNodeValue() + "</" + node.getNodeName() + ">";
+    public static void printTags(NodeList tags, String requiredTag){
+        // Просматриваем все подэлементы корневого
+        for (int i = 0; i < tags.getLength(); i++){
+            Node tag = tags.item(i);
+
+            //сравниваем название тег с искомым
+            if (tag.getNodeName().equals(requiredTag)) {
+                System.out.println(tagConstructor(tag));
+            }
+
+            //если у тега есть дочерние теги, то выводим на экран и их тоже
+            if (tag.hasChildNodes()) printTags(tag.getChildNodes(), requiredTag);
+        }
+    }
+
+    public static String tagConstructor(Node node){
+        if (node.getNodeType() == Node.TEXT_NODE) return node.getNodeValue();
+
+        StringBuilder builderStringTag = new StringBuilder();
+        builderStringTag.append("<").append(node.getNodeName());// <tag
+
+        //получаем атрибуты и собираем их в builderStringTag
+        if (node.hasAttributes() == true){
+            NamedNodeMap attributes = node.getAttributes();
+            builderStringTag.append(" ");// ставим пробел после тега если есть атрибуты
+            for (int i = attributes.getLength() - 1; i >= 0; i--){
+                Node attr = attributes.item(i);
+                builderStringTag.append(attr.getNodeName()).append("=\"").append(attr.getNodeValue()).append("\"");
+                //если атрибут не последний, то после него ставим пробел
+                if ( i!=0 ) builderStringTag.append(" ");
+            }
+        }
+        builderStringTag.append(">");// >
+
+        //если у тега есть дочерние элементы, то вызываем этот же метод
+        if (node.hasChildNodes()){
+            String childTag = "";
+            NodeList listChildTags = node.getChildNodes();
+            for (int i = 0; i < listChildTags.getLength(); i++){
+                childTag = tagConstructor(listChildTags.item(i));
+                builderStringTag.append(childTag);
+            }
+
+        }
+
+        builderStringTag.append("</").append(node.getNodeName()).append(">");// </span>
+
+        return builderStringTag.toString();
     }
 }

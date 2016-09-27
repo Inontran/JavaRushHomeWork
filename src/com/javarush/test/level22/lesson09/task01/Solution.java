f59ff35da7c72f66a2489807e85673ca8bbc06f9
Inontran <braginsasha1995@mail.ru>
2016-09-27 23:48:41 +0900

    level22.lesson09. simple save

--- src/com/javarush/test/level22/lesson09/task01/Solution.java
+++ src/com/javarush/test/level22/lesson09/task01/Solution.java
@@ -27,29 +27,31 @@ public class Solution {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader fileReader = new BufferedReader(new FileReader(consoleReader.readLine()));
 
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while (fileReader.ready()) stringBuilder.append(fileReader.readLine()).append(System.lineSeparator());
+
         String previous = "";
-        StringBuilder nextWord;
+        String currentString;
         Pair newPair;
-        StringTokenizer tokenizer = new StringTokenizer(fileReader.readLine(), " \t\r\n");
+        StringTokenizer tokenizer = new StringTokenizer(stringBuilder.toString(), " " + System.lineSeparator());
+
         if (tokenizer.hasMoreTokens()) previous = tokenizer.nextToken();
         while (tokenizer.hasMoreTokens())
         {
-            nextWord = new StringBuilder(tokenizer.nextToken());
-            if (previous.equals(nextWord.reverse()))
+            currentString = tokenizer.nextToken();
+            stringBuilder = new StringBuilder(currentString);
+            if (stringBuilder.reverse().toString().equals(previous))
             {
                 newPair = new Pair();
                 newPair.first = previous;
-                newPair.second = nextWord.toString();
-                result.add(newPair);
+                newPair.second = stringBuilder.toString();
+                if (!result.contains(newPair)) result.add(newPair);
             }
-            previous = nextWord.toString();
+            previous = currentString;
         }
 
-        for (int i = 0; i < result.size(); i++)
-        {
-            System.out.print(result.get(i) + " ");
-        }
-
+        for (int i = 0; i < result.size(); i++) System.out.println(result.get(i).first + " " + result.get(i).second);
 
         if (consoleReader!=null) consoleReader.close();
         if (fileReader!=null) fileReader.close();
@@ -66,6 +68,27 @@ public class Solution {
                     second == null && first != null ? first :
                     first.compareTo(second) < 0 ? first + " " + second : second + " " + first;
 
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) return true;
+            if (!(o instanceof Pair)) return false;
+
+            Pair pair = (Pair) o;
+
+            if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            return second != null ? second.equals(pair.second) : pair.second == null;
+
+        }
+
+        @Override
+        public int hashCode()
+        {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
         }
     }
 
---
+++ src/com/javarush/test/level22/lesson09/task02/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level22.lesson09.task02;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* Формируем Where
+Сформируйте часть запроса WHERE используя StringBuilder.
+Если значение null, то параметр не должен попадать в запрос.
+Пример:
+{"name", "Ivanov", "country", "Ukraine", "city", "Kiev", "age", null}
+Результат:
+"name = 'Ivanov' and country = 'Ukraine' and city = 'Kiev'"
+*/
+public class Solution {
+
+    public static StringBuilder getCondition(Map<String, String> params) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String,String> pair : params.entrySet())
+        {
+            if (pair.getValue() != null) stringBuilder.append(pair.getKey()).append(" = '").append(pair.getValue()).append("'").append(" and ");
+        }
+        stringBuilder.delete(stringBuilder.length()-5, stringBuilder.length());
+        return stringBuilder;
+    }
+}

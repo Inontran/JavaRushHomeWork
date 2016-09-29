b6e7616c5c059e0cae06730968cdea422adb2614
Inontran <braginsasha1995@mail.ru>
2016-09-29 12:39:17 +0900

    level22.lesson13. simple save

--- src/com/javarush/test/level22/lesson13/task02/Solution.java
+++ src/com/javarush/test/level22/lesson13/task02/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level22.lesson13.task02;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /* Смена кодировки
@@ -10,6 +12,18 @@ public class Solution {
     static String win1251TestString = "РќР°СЂСѓС€РµРЅРёРµ РєРѕРґРёСЂРѕРІРєРё РєРѕРЅСЃРѕР»Рё?"; //only for your testing
 
     public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(args[0]);
+        FileOutputStream outputStream = new FileOutputStream(args[1]);
+        byte[] buffer = new byte[1024];
+        String s = "";
+        while (fileInputStream.available() > 0)
+        {
+            fileInputStream.read(buffer);
+            s = new String(buffer, "UTF-8");
+            outputStream.write(s.getBytes());
+        }
 
+        if (fileInputStream!=null) fileInputStream.close();
+        if (outputStream!=null) outputStream.close();
     }
 }

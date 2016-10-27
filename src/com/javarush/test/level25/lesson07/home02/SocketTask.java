12937e3d82317d67e936491ae95618291cbef45d
Inontran <pakim.wesi@gail.com>
2016-10-27 23:31:50 +1000

    level25.lesson07. simple save

--- src/com/javarush/test/level25/lesson07/home02/SocketTask.java
+++ src/com/javarush/test/level25/lesson07/home02/SocketTask.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level25.lesson07.home02;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.RunnableFuture;
@@ -13,6 +14,14 @@ public abstract class SocketTask<T> implements CancellableTask<T> {
 
     public synchronized void cancel() {
         //close all resources here
+        try
+        {
+            socket.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public RunnableFuture<T> newTask() {
@@ -20,6 +29,18 @@ public abstract class SocketTask<T> implements CancellableTask<T> {
             public boolean cancel(boolean mayInterruptIfRunning) {
                 //close all resources here by using proper SocketTask method
                 //call super-class method in finally block
+                try
+                {
+                    socket.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    super.cancel(mayInterruptIfRunning);
+                }
                 return false;
             }
         };
